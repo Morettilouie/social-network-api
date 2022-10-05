a8f1a801c5770a12154d45bb18dff0ad51d6440d
Louie Moretti <morettilouie@gmail.com>
2022-10-05 00:50:06 -0500

    added models

---
+++ models/Username.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    thoughts: [],
+    friends: []
+});
+
+// create the Username model using the UsernameSchema
+const User = model('Username', UserSchema);
+
+
+// export the Username model
+module.exports = User;
